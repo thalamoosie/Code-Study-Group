eb6623b9d75b9c20233f55dfc28d877cf546790e
Latif Yahia <latif_yahia@woebothealth.com>
2023-05-10 12:44:17 +0100

    updated check in addTwoPositiveNumbers function

--- unit-testing/functions.js
+++ unit-testing/functions.js
@@ -1,5 +1,5 @@
 function addTwoPostitiveNumber(num1, num2) {
-    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num1 !== 'number') {
+    if (num1 < 0 || num2 < 0 || typeof num1 !== 'number' || typeof num2 !== 'number') {
         return 0
     }
 
